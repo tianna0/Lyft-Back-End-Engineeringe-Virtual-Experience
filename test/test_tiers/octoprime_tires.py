33da95ffa4355c12ce5a33bd3fa633a02740803f
tianna0 <144874312+tianna0@users.noreply.github.com>
2023-09-29 00:03:56 -0500

    Delete test/test_tiers/octoprime_tires.py

--- test/test_tiers/octoprime_tires.py
+++
@@ -1,9 +0,0 @@
-from tires.tires import Tires
-
-
-class OctoprimeTires(Tires):
-    def __init__(self, tire_wear):
-        self.tire_wear = tire_wear
-
-    def needs_service(self):
-        return sum(self.tire_wear) >= 3.0
